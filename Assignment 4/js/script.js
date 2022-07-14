220f09e411e69c1d69baf1539cb4a79abe93caba
Jaspreet Kaur <91208959+JaspreetKaur09@users.noreply.github.com>
2022-07-14 12:59:56 -0700

    first commit

---
+++ Assignment 4/js/script.js
@@ -0,0 +1,31 @@
+/*
+   Jaspreet kaur
+*/
+$(function () {
+  function getCurrentLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success,showError);
+    } else {
+      $('#error').text("Geolocation is not supported on your browser!");
+    }
+    function success(position) {
+      $("#latitude").val(position.coords.latitude);
+      $("#longitude").val(position.coords.longitude);
+    }
+    function showError(error)
+    {
+      $('#error').text("Location permission denied");
+    }
+}
+  $("#btnP").on("click", function () {
+    getCurrentLocation();
+  });
+  $("#btnSubmit").on("click", function () {
+    localStorage.setItem("firstname", $("#firstname").val());
+    localStorage.setItem("lastname", $("#lastname").val());
+    localStorage.setItem("longitude", $("#longitude").val());
+    localStorage.setItem("latitude", $("#latitude").val());
+    $('#success').text("Information submitted successfully");
+  });
+
+});
